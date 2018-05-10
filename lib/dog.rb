039c7ca75b884563c01112bf24fa310fa7186143
Angie Kassab <anasab12@gmail.com>
2018-05-10 17:24:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,3 @@
-class Person
-  def name
-    @name
-  end
-
-  def name= (new_name)
-    @name = new_name
-  end
-
-  def job
-    @job
-  end
-
-  def job= (new_job)
-    @job = new_job
-  end
-end
-
-=begin
-
 class Dog
 def name=(name)
   @name=name
@@ -28,5 +8,3 @@ def breed (breed)
 end
 
 end
-
-=end
