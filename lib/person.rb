79aa68a01ae356ad4f4cbaeebb13c93435149339
Angie Kassab <anasab12@gmail.com>
2018-05-10 17:35:28 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,16 +3,16 @@ class Person
     @name
   end
 
-  def name= (new_name)
-    @name = new_name
+  def name= (person_name)
+    @name = person_name
   end
 
   def job
     @job
   end
 
-  def job= (new_job)
-    @job = new_job
+  def job= (person_job)
+    @job = person_job
   end
 end
 
